dcfcba8ed15f351ca1d4a7759240745aed75fb9c
Brian Sam-Bodden <bsbodden@integrallis.com>
2026-08-29 17:35:20 -0700

    chore(release): prepare ModelJars 0.1.24

--- build.gradle.kts
+++ build.gradle.kts
@@ -1954,7 +1954,7 @@ allprojects {
     version =
         providers
             .gradleProperty("modeljarsVersion")
-            .orElse("0.1.23-SNAPSHOT")
+            .orElse("0.1.24-SNAPSHOT")
             .get()
 }
 
